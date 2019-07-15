7c17df093609d57596762e7ae4ca2b3115e97eae
z0aiyx <2509235926@qq.com>
2019-07-15 08:41:08 +0800

    移动端配置

--- wymusic/src/App.js
+++ wymusic/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import "./App.css"
 class App extends React.Component {
 	
 	render() {
 		return (
-			<div>
-				
+			<div className="box">
+				<p>你好</p>
 			</div>
 		)
 	}
